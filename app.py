9dbecf372c2357967dad0b27c928511073e1562c
Maximilian Freitag <46624616+MaximilianFreitag@users.noreply.github.com>
2022-04-15 18:01:10 +0200

    Update app.py

--- app.py
+++ app.py
@@ -61,7 +61,7 @@ st.title("Image Classification with Teachable Machine Learning")
 st.header("Normal X Ray Vs Pneumonia X Ray")
 st.text("Upload a X Ray to detect it is normal or has pneumonia")
 # file upload and handling logic
-uploaded_file = st.file_uploader("Choose a X Ray Image", type="jpeg")
+uploaded_file = st.file_uploader("Choose a X Ray Image", type="png")
 if uploaded_file is not None:
     image = Image.open(uploaded_file).convert('RGB')
 #image = Image.open(img_name).convert('RGB')
@@ -70,7 +70,7 @@ if uploaded_file is not None:
     st.write("Classifying a X Ray Image - Normal Vs Pneumonia.........hold tight")
 
 
-    label = teachable_machine_classification(image, 'Users/converted_keras/keras_model.h5')
+    label = teachable_machine_classification(image, '/mnemo_ai/converted_keras/keras_model.h5')
 
     if label == 1:
         st.write("This X ray looks like having pneumonia.It has abnormal opacification.Needs further investigation by a Radiologist/Doctor.")
