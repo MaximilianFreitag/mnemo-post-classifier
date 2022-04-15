7a9c55e2a931150cfe2b14045dec8abd295f99b8
Maximilian Freitag <46624616+MaximilianFreitag@users.noreply.github.com>
2022-04-15 17:39:46 +0200

    Update app.py

--- app.py
+++ app.py
@@ -15,12 +15,16 @@ col1, col2, col3, col4, col5 = st.columns([1,1,5,1,1])
 
 def teachable_machine_classification(img, weights_file):
     # Load the model
-    model = keras.models.load_model(weights_file)
+    model = load_model('keras_model.h5')
 
     # Create the array of the right shape to feed into the keras model
+    # The 'length' or number of images you can put into the array is
+    # determined by the first position in the shape tuple, in this case 1.
     data = np.ndarray(shape=(1, 224, 224, 3), dtype=np.float32)
-    image = img
-    #image sizing
+    # Replace this with the path to your image
+    image = Image.open('<IMAGE_PATH>')
+    #resize the image to a 224x224 with the same strategy as in TM2:
+    #resizing the image to be at least 224x224 and then cropping from the center
     size = (224, 224)
     image = ImageOps.fit(image, size, Image.ANTIALIAS)
 
@@ -28,13 +32,12 @@ def teachable_machine_classification(img, weights_file):
     image_array = np.asarray(image)
     # Normalize the image
     normalized_image_array = (image_array.astype(np.float32) / 127.0) - 1
-
     # Load the image into the array
     data[0] = normalized_image_array
 
     # run the inference
     prediction = model.predict(data)
-    return np.argmax(prediction) # return position of the highest probability
+    return prediction
 
 
 
