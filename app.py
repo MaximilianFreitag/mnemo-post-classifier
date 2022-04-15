467661e86d471be6720cd240628964bec600c3f9
Maximilian Freitag <46624616+MaximilianFreitag@users.noreply.github.com>
2022-04-15 18:13:49 +0200

    Update app.py

--- app.py
+++ app.py
@@ -16,63 +16,86 @@ from keras.models import load_model
 model = tf.keras.models.load_model('model.h5')
 
 
-def teachable_machine_classification(img, uploaded_file):
-    # Disable scientific notation for clarity
-    np.set_printoptions(suppress=True)
 
-    # Load the model
-    model = keras.models.load_model(uploaded_file)
+#Favicon and Header
+st.set_page_config(
+        page_title='Mnemo Classifier                 ',
+        page_icon="🤖"
+        )
 
-    # Create the array of the right shape to feed into the keras model
-    # The 'length' or number of images you can put into the array is
-    # determined by the first position in the shape tuple, in this case 1.
-    data = np.ndarray(shape=(1, 224, 224, 3), dtype=np.float32)
 
-    # Replace this with the path to your image
-    image = img
-    # image = Image.open(img_name).convert('RGB')
-    # image = cv2.imread(image)
-
-    # resize the image to a 224x224 with the same strategy as in TM2:
-    # resizing the image to be at least 224x224 and then cropping from the center
-    size = (224, 224)
-    image = ImageOps.fit(image, size, Image.ANTIALIAS)
-
-    # turn the image into a numpy array
-    image_array = np.asarray(image)
-
-    # display the resized image
-    image.show()
-
-    # Normalize the image
-    normalized_image_array = (image_array.astype(np.float32) / 127.0) - 1
-
-    # Load the image into the array
-    data[0] = normalized_image_array
-
-    # run the inference
-    prediction = model.predict(data)
-    #print(prediction)
-    return np.argmax(prediction)
+hide_streamlit_style = """
+            <style>
+            #MainMenu {visibility: hidden;}
+            footer {visibility: hidden;}
+            </style>
+            """
+st.markdown(hide_streamlit_style, unsafe_allow_html=True) 
 
 
 
-st.title("Mnemo Post Classifier")
-st.header("Take a screenshot from my instagram posts and see if my trained Ai model is right at classiyfing my style of post")
-
-# file upload and handling logic
-uploaded_file = st.file_uploader("Choose a X Ray Image", type="png")
-if uploaded_file is not None:
-    image = Image.open(uploaded_file).convert('RGB')
-#image = Image.open(img_name).convert('RGB')
-    st.image(image, caption='Uploaded a X Ray IMage.', use_column_width=True)
-    st.write("")
-    st.write("Classifying a X Ray Image - Normal Vs Pneumonia.........hold tight")
+col1, col2, col3, col4, col5 = st.columns([1,1,5,1,1])
 
 
-    label = teachable_machine_classification(image, 'model.h5')
+with col3:
 
-    if label == 1:
-        st.write("This doens't look like a MNEMO post!")
-    else:
-        st.write("This is likely to be a MNEMO post")
+    def teachable_machine_classification(img, uploaded_file):
+        # Disable scientific notation for clarity
+        np.set_printoptions(suppress=True)
+
+        # Load the model
+        model = keras.models.load_model(uploaded_file)
+
+        # Create the array of the right shape to feed into the keras model
+        # The 'length' or number of images you can put into the array is
+        # determined by the first position in the shape tuple, in this case 1.
+        data = np.ndarray(shape=(1, 224, 224, 3), dtype=np.float32)
+
+        # Replace this with the path to your image
+        image = img
+        # image = Image.open(img_name).convert('RGB')
+        # image = cv2.imread(image)
+
+        # resize the image to a 224x224 with the same strategy as in TM2:
+        # resizing the image to be at least 224x224 and then cropping from the center
+        size = (224, 224)
+        image = ImageOps.fit(image, size, Image.ANTIALIAS)
+
+        # turn the image into a numpy array
+        image_array = np.asarray(image)
+
+        # display the resized image
+        image.show()
+
+        # Normalize the image
+        normalized_image_array = (image_array.astype(np.float32) / 127.0) - 1
+
+        # Load the image into the array
+        data[0] = normalized_image_array
+
+        # run the inference
+        prediction = model.predict(data)
+        #print(prediction)
+        return np.argmax(prediction)
+
+
+
+    st.title("Mnemo Post Classifier")
+    st.header("Take a screenshot from my instagram posts and see if my trained Ai model is right at classiyfing my style of post")
+
+    # file upload and handling logic
+    uploaded_file = st.file_uploader("Choose a X Ray Image", type="png")
+    if uploaded_file is not None:
+        image = Image.open(uploaded_file).convert('RGB')
+    #image = Image.open(img_name).convert('RGB')
+        st.image(image, caption='Uploaded a X Ray IMage.', use_column_width=True)
+        st.write("")
+        st.write("Classifying a X Ray Image - Normal Vs Pneumonia.........hold tight")
+
+
+        label = teachable_machine_classification(image, 'model.h5')
+
+        if label == 1:
+            st.write("This doens't look like a MNEMO post!")
+        else:
+            st.write("This is likely to be a MNEMO post")
